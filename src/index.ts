f7af0c67776a3ddae97102258d0fa42c6b92c2a3
Xifre Font <xifre.font.97@gmail.com>
2022-08-11 18:07:35 +0200

    Set up port at env and express server

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,11 @@ import express from "express";
 
 const debug = Debug("my-docs:index");
 
+const app = express();
+const port = process.env.PORT ?? 4000;
+
+app.listen(port, () => {
+  debug("Server on");
+});
+
 debug("Server");
